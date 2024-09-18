b901ee247791d3acaa81ad748ac730de23c9e3eb
Dovahrae <167934960+Dovahrae@users.noreply.github.com>
2024-09-18 18:35:07 -0400

    created page

---
+++ src/api.js
@@ -0,0 +1,24 @@
+// let backendURL = () => {
+//     let url = "";
+//     if (window.location.href === "http://localhost:3000") {
+//         url = "http://localhost:3001";
+//     } else {
+//         url = "https:/capstone-backend-withered-frog-1128-db.flycast";
+//     }
+// };
+
+// add "let backendURL = () =>" each time you import it for something
+
+// export default backendURL
+// import backendURL from "./api"
+
+// DON'T FORGET TO NPM RUN BUILD to get new code to run online
+// then netlify deploy --prod
+
+// nates finished version
+// export const backendUrl = () => {
+//     if (window.location.href === "http://localhost:3000") {
+//         return "http://localhost:3001"
+//     }
+//     return "https://capstone-backend-young-pond-3810.fly.dev"
+// }

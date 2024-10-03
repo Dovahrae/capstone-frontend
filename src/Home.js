245ecb1e6866301a887bf7ef937b5d58487d9fdf
Dovahrae <167934960+Dovahrae@users.noreply.github.com>
2024-10-03 11:10:56 -0400

    added a description
    of how to use the app on the homepage

--- src/Home.js
+++ src/Home.js
@@ -10,19 +10,36 @@ const ToggleButton = () => {
     };
 
     return (
-        <button
-            onClick={handleToggle}
-            style={{
-                backgroundColor: isSelected ? "green" : "lightgray", // Change color based on selection
-                color: "white",
-                border: "none",
-                padding: "10px 20px",
-                cursor: "pointer",
-                transition: "background-color 0.3s", // Smooth transition for color change
-            }}
-        >
-            Testing
-        </button>
+        <div>
+            <h1>Welcome to my Stardew Valley Tracker!</h1>
+            <h3>
+                This tracker is so simple to use! Just login (to save your
+                progress),
+            </h3>
+            <h3>
+                use the navigation bar at the top to find the room you've
+                donated to,
+            </h3>
+            <h3>and select the item you've donated!</h3>
+            <h3>Accidentally click the wrong item? No problem!</h3>
+            <h3>
+                Simply click the item again to uncheck it. It's that simple!
+            </h3>
+            <h3>Give it a try below and happy harvesting!</h3>
+            <button
+                onClick={handleToggle}
+                style={{
+                    backgroundColor: isSelected ? "green" : "lightgray", // Change color based on selection
+                    color: "white",
+                    border: "none",
+                    padding: "10px 20px",
+                    cursor: "pointer",
+                    transition: "background-color 0.3s", // Smooth transition for color change
+                }}
+            >
+                Try it out!
+            </button>
+        </div>
     );
 };
 
